4f1de2df11847959b26d7d989202fb36a46042d0
NovikE <34532613+NovikE@users.noreply.github.com>
2017-12-14 09:04:36 +0300

    the first project

---
+++ src/MainApp.java
@@ -0,0 +1,221 @@
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.out.println("3+3");
+
+		int x = 0;
+
+		for (int i = 0; i <= 100; i++) {
+			// if (i%2 == 0) {
+			x = x + i;
+			// System.out.println(x);
+			// System.out.println(i);
+			// }
+		}
+		System.out.println(x);
+
+		String month1 = "Jan";
+		String month2 = "Feb";
+		String month3 = "Mar";
+		String month4 = "Apr";
+		String month5 = "May";
+		String month6 = "Jun";
+		String month7 = "Jul";
+		String month8 = "Aug";
+		String month9 = "Sep";
+		String month10 = "Oct";
+		String month11 = "Nov";
+		String month12 = "Dec";
+
+		for (int i = 1; i <= 12; i++) {
+
+			if (i == 1) {
+				System.out.println(month1);
+			}
+			if (i == 2) {
+				System.out.println(month2);
+			}
+			if (i == 3) {
+				System.out.println(month3);
+			}
+			if (i == 4) {
+				System.out.println(month4);
+			}
+			if (i == 5) {
+				System.out.println(month5);
+			}
+			if (i == 6) {
+				System.out.println(month6);
+			}
+			if (i == 7) {
+				System.out.println(month7);
+			}
+			if (i == 8) {
+				System.out.println(month8);
+			}
+			if (i == 9) {
+				System.out.println(month9);
+			}
+			if (i == 10) {
+				System.out.println(month10);
+			}
+			if (i == 11) {
+				System.out.println(month11);
+			}
+			if (i == 12) {
+				System.out.println(month12);
+			}
+
+		}
+
+		boolean b1 = true;
+		int j = 3;
+
+		while (b1) {
+			if (j == 1) {
+				b1 = false;
+			}
+			System.out.println(j);
+			j--;
+		}
+
+		int p = 0;
+		while (p <= 10) {
+			System.out.println(p);
+			p++;
+		}
+
+		int i = 1;
+
+		p = 0;
+		x = 0;
+		while (p <= 100) {
+			x = x + p;
+			p++;
+		}
+		System.out.println(x);
+
+		i = 1;
+
+		while (i <= 12) {
+			if (i == 1) {
+				System.out.println(month1);
+			}
+			if (i == 2) {
+				System.out.println(month2);
+			}
+			if (i == 3) {
+				System.out.println(month3);
+			}
+			if (i == 4) {
+				System.out.println(month4);
+			}
+			if (i == 5) {
+				System.out.println(month5);
+			}
+			if (i == 6) {
+				System.out.println(month6);
+			}
+			if (i == 7) {
+				System.out.println(month7);
+			}
+			if (i == 8) {
+				System.out.println(month8);
+			}
+			if (i == 9) {
+				System.out.println(month9);
+			}
+			if (i == 10) {
+				System.out.println(month10);
+			}
+			if (i == 11) {
+				System.out.println(month11);
+			}
+			if (i == 12) {
+				System.out.println(month12);
+			}
+			i++;
+		}
+
+		i = 1;
+		while (i <= 10) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+
+		i = 2;
+		while (i <= 10) {
+			System.out.println(i);
+			i = i + 2;
+		}
+
+		i = 1;
+		do {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+			i++;
+		} while (i <= 10);
+
+		i = 2;
+		do {
+			System.out.println(i);
+			i = i + 2;
+		} while (i <= 10);
+
+		i = 0;
+		x = 0;
+
+		do {
+			x = x + i;
+			i++;
+		} while (i <= 100);
+		System.out.println(x);
+
+		i = 1;
+		do {
+			if (i == 1) {
+				System.out.println(month1);
+			}
+			if (i == 2) {
+				System.out.println(month2);
+			}
+			if (i == 3) {
+				System.out.println(month3);
+			}
+			if (i == 4) {
+				System.out.println(month4);
+			}
+			if (i == 5) {
+				System.out.println(month5);
+			}
+			if (i == 6) {
+				System.out.println(month6);
+			}
+			if (i == 7) {
+				System.out.println(month7);
+			}
+			if (i == 8) {
+				System.out.println(month8);
+			}
+			if (i == 9) {
+				System.out.println(month9);
+			}
+			if (i == 10) {
+				System.out.println(month10);
+			}
+			if (i == 11) {
+				System.out.println(month11);
+			}
+			if (i == 12) {
+				System.out.println(month12);
+			}
+			i++;
+
+		} while (i <= 12);
+
+	}
+}
